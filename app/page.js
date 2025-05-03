2180087d6f34b7a5ae7cc1a53f1eeb68f0b8a5c0
Noah Amoo <yawnoah3n@yahoo.com>
2025-05-03 02:16:52 +0100

    Created the frontend user registration form on the Homepage

--- app/page.js
+++ app/page.js
@@ -4,9 +4,14 @@ import Link from 'next/link'
 export default function page() {
   return (
     <div>
-      <h1>Hello</h1>
-      <Link href='/login'>Login Page</Link>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt expedita quod officiis recusandae voluptatem pariatur vero esse ullam, quasi eveniet!</p>
+      <p className='text-center text-2xl text-gray-600 mb-5'>Don&rsquo;t have an account? <strong>Create One</strong></p>
+      <form action="" className='max-w-xs mx-auto'>
+        <div className="mb-3">
+        <input name='username' autoComplete='off' placeholder="Username" type="text" className="input input-bordered w-full max-w-xs mb-2" />
+        <input name='password' autoComplete='off' placeholder="Password" type="password" className="input input-bordered w-full max-w-xs mb-2" />
+        <button className='btn btn-primary'>Create Account</button>
+        </div>
+      </form>
     </div>
   )
 }
