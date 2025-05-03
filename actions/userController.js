117a244a49db02dadde9c263a1e7836a3af2dc53
Noah Amoo <yawnoah3n@yahoo.com>
2025-05-03 09:57:22 +0100

    Completed register user validation procedure in teh UserController file

--- actions/userController.js
+++ actions/userController.js
@@ -1,9 +1,28 @@
 "use server"
 
-export const register = async function() {
-    console.log('Registering user on the server')
-    return {
-        skyColor: 'blue',
-        grassColor: 'green',
+export const register = async function(prevState, FormData) {
+    const errors = {}
+
+    const ourUser = {
+        username: FormData.get("username"),
+        password: FormData.get("password"),
+
+    }
+
+    if (typeof ourUser.username !== "string") ourUser.username = ""
+    if (typeof ourUser.password !== "string") ourUser.password = ""
+
+    ourUser.username = ourUser.username.trim()
+    ourUser.password = ourUser.password.trim()
+
+    // Validation
+    if (ourUser.username.length < 3) errors.username = "Username must be at least 3 characters long"
+    if (ourUser.username.length > 30) errors.username = "Username cannot be longer than 30 characters"
+
+    if (errors.username || errors.password) {
+        return {
+            errors: errors,
+            success: false,
+        }
     }
 }
--- components/RegisterForm.jsx
+++ components/RegisterForm.jsx
@@ -1,12 +1,16 @@
 "use client"
 
+import { useFormState, useFormStatus } from 'react-dom'
 import { register } from '@/actions/userController'
 
 import React from 'react'
 
 export default function RegisterForm() {
+    const [formState, formAction] = useFormState(register, {})
+    console.log('Form state:', formState)
+    
   return (
-    <form action={ register } className='max-w-xs mx-auto'>
+    <form action={ formAction } className='max-w-xs mx-auto'>
         <div className="mb-3">
         <input name='username' autoComplete='off' placeholder="Username" type="text" className="input input-bordered w-full max-w-xs mb-2" />
         <input name='password' autoComplete='off' placeholder="Password" type="password" className="input input-bordered w-full max-w-xs mb-2" />
