c52490236f8303aff78b31b361ee49d97f8aa474
Noah Amoo <yawnoah3n@yahoo.com>
2025-05-03 01:18:08 +0100

    Initialized Navbar

---
+++ app/login/page.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+      <h1 className="text-red-500">Login Page</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi rerum earum itaque veniam illum rem aut expedita, repudiandae, harum natus porro enim sint? Temporibus est praesentium aspernatur quas aut eveniet.</p>
+    </div>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// /** @type {import('tailwindcss').Config} */
+// export const content = [
+//   "./app/**/*.{js,ts,jsx,tsx}", // Covers all files in the app directory
+//   "./components/**/*.{js,ts,jsx,tsx}", // Covers a components directory if it exists
+//   "./pages/**/*.{js,ts,jsx,tsx}", // Covers a pages directory if it exists
+//   "./lib/**/*.{js,ts,jsx,tsx}", // Covers a lib directory if it exists
+// ];
+// export const theme = {
+//   extend: {},
+// };
+// export const plugins = [require('daisyui')];
+
