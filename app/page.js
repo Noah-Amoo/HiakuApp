c1f5f3b6c40289744d61a12a44f9f313f98daaa3
Noah Amoo <yawnoah3n@yahoo.com>
2025-05-06 11:38:46 +0100

    Cookie function written to automatically login a registered member

--- app/page.js
+++ app/page.js
@@ -1,11 +1,20 @@
 import React from 'react'
 import RegisterForm from '@/components/RegisterForm'
+import getUserFromCookie from '@/lib/getUser'
 
-export default function page() {
+export default async function page() {
+  const user = await getUserFromCookie()
   return (
     <div>
-      <p className='text-center text-2xl text-gray-600 mb-5'>Don&rsquo;t have an account? <strong>Create One</strong></p>
-      <RegisterForm />
+      {user && (
+        <p>Welcome, you are logged in</p>
+      )}
+      {!user && (
+      <div>
+        <p className='text-center text-2xl text-gray-600 mb-5'>Don&rsquo;t have an account? <strong>Create One</strong></p>
+        <RegisterForm />
+      </div>
+      )}
     </div>
   )
 }
