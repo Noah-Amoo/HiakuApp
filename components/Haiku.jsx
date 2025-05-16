6579c7261cc758d9403185abf222ddbb00603431
Noah Amoo <yawnoah3n@yahoo.com>
2025-05-16 17:08:07 +0100

    Added Cloudinary's special image padding effect

--- components/Haiku.jsx
+++ components/Haiku.jsx
@@ -6,25 +6,31 @@ import { deleteHaiku } from '@/actions/haikuController'
 import { CldImage } from 'next-cloudinary';
 
 export default function Haiku(props) {
-  return (
-    <div className='bg-gray-100 p-4 mb-4 rounded-lg'>
-        <CldImage
-            width="650"
-            height="300"
-            src={props.haiku.photo}
-            sizes="650px"
-            alt="Description of my image"
-        />
-        <p className='text-lg'>{props.haiku.line1}</p>
-        <p className='text-lg'>{props.haiku.line2}</p>
-        <p className='text-lg'>{props.haiku.line3}</p>
-        <Link href={`/edit-haiku/${props.haiku._id.toString()}`}>Edit</Link>
-        <form action={ deleteHaiku }>
-            <input name='id' type="hidden" defaultValue={props.haiku._id.toString()} />
-            <button>Delete</button>
-        </form>
-        
-        <hr className='border-t-2 border-gray-300 mt-4' />
-    </div>
-  )
+    return (
+        <div className='bg-gray-100 p-4 mb-4 rounded-lg'>
+            <CldImage
+                width="650"
+                height="300"
+                src={props.haiku.photo}
+                sizes="650px"
+                alt="Description of my image"
+                // Using cloudinary's AI to crop the image
+                crop={{
+                    type: 'pad',
+                    source: 'true'
+                }}
+                fillBackground
+            />
+            <p className='text-lg'>{props.haiku.line1}</p>
+            <p className='text-lg'>{props.haiku.line2}</p>
+            <p className='text-lg'>{props.haiku.line3}</p>
+            <Link href={`/edit-haiku/${props.haiku._id.toString()}`}>Edit</Link>
+            <form action={deleteHaiku}>
+                <input name='id' type="hidden" defaultValue={props.haiku._id.toString()} />
+                <button>Delete</button>
+            </form>
+
+            <hr className='border-t-2 border-gray-300 mt-4' />
+        </div>
+    )
 }
